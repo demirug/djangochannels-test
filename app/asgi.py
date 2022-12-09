3e5bd2de2d39d95603a644557ab95921c77e27c7
demirug <pm.antondolia@gmail.com>
2022-12-09 18:28:15 +0200

    Registered channel consumer

--- app/asgi.py
+++ app/asgi.py
@@ -10,11 +10,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
 from app.ws_urls import urlpatterns as websocket_urlpatterns
+from apps.channelsend.consumers import ChannelTest
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
@@ -22,5 +23,8 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(URLRouter([path("ws/", URLRouter(websocket_urlpatterns))]))
+    "websocket": AuthMiddlewareStack(URLRouter([path("ws/", URLRouter(websocket_urlpatterns))])),
+    "channel": ChannelNameRouter({
+        "test_channel": ChannelTest.as_asgi(),
+    }),
 })
